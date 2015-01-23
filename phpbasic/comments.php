7015e69f1c8066c71069bae147d83e0a87e0615f
tri <tri@local.com>
2015-01-23 09:07:37 +0000

    phpbasic

--- phpbasic/comments.php
+++ phpbasic/comments.php
@@ -7,7 +7,7 @@ echo 'This is a test'; // This is a one-line c++ style comment
 /* This is a multi line comment
    yet another line of comment */
 echo 'This is yet another test';
-echo 'One Final Test'; # This is a one-line shell-style comment
+echo 'One Final Test'; # this comment ok
 ?>
 
 
@@ -15,7 +15,7 @@ echo 'One Final Test'; # This is a one-line shell-style comment
 // This is a single line comment
 
 # This is also a single line comment
-# New line comment ?> this line will not printed
+# New line comment - this line will printed ?> 
 
 <?php
 /*
@@ -24,7 +24,8 @@ comment
 */
 
 /* this is also
-commented */ but this not - parse error T_STRING */
+commented */ 
+
 ?>
 
 <?php
